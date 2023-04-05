4b5631e063ad990a6fab4fb4be051c813eb181d2
2dukes <ruipinto02@hotmail.com>
2023-04-05 17:05:25 +0100

    Update: Windows dockerfile.

---
+++ project/windows/cmds.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t ubuntukvm_windows -f Dockerfile .
+
+docker run --privileged -it --name kvmcontainer1 --device=/dev/kvm --device=/dev/net/tun -v /sys/fs/cgroup:/sys/fs/cgroup:rw --cap-add=NET_ADMIN --cap-add=SYS_ADMIN ubuntukvm_windows bash
