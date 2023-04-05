cdb2ea0bc190f345f91fd5476f87d79140eeff7d
2dukes <ruipinto02@hotmail.com>
2023-04-05 17:53:38 +0100

    Update: Image construction structure.

--- project/windows/cmds.sh
+++ project/windows/cmds.sh
@@ -2,4 +2,4 @@
 
 docker build -t ubuntukvm_windows -f Dockerfile .
 
-docker run --privileged -it --name kvmcontainer1 --device=/dev/kvm --device=/dev/net/tun -v /sys/fs/cgroup:/sys/fs/cgroup:rw --cap-add=NET_ADMIN --cap-add=SYS_ADMIN ubuntukvm_windows bash
+docker run --privileged -it --name kvmcontainer --device=/dev/kvm --device=/dev/net/tun -v /sys/fs/cgroup:/sys/fs/cgroup:rw --cap-add=NET_ADMIN --cap-add=SYS_ADMIN ubuntukvm_windows bash
